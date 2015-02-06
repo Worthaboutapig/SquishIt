583597cd6e99ef6f2b147f84f97090bfa193589f
Worthaboutapig <jeremy@savetrees.co.uk>
2015-02-06 16:04:49 +0000

    Merge

---
+++ SquishIt.Framework/HttpRuntime.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Caching;
+
+namespace SquishIt.Framework
+{
+	internal class HttpRuntime
+	{
+		internal static string appDomainAppPath;
+		internal static string AppDomainAppPath
+		{
+			get { return appDomainAppPath ?? System.Web.HttpRuntime.AppDomainAppPath; } 
+		}
+
+		internal static string appDomainAppVirtualPath;
+		internal static string AppDomainAppVirtualPath
+		{
+			get { return appDomainAppVirtualPath ?? System.Web.HttpRuntime.AppDomainAppVirtualPath; } 
+		}
+
+		internal static Cache cache;
+		internal static Cache Cache
+		{
+			get { return cache ?? System.Web.HttpRuntime.Cache; } 
+		}
+	}
+}
---
+++ SquishIt.Tests/Helpers/HttpRuntimeScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Caching;
+
+namespace SquishIt.Tests.Helpers
+{
+	public class HttpRuntimeScope : IDisposable
+    {
+		public HttpRuntimeScope(string appDomainAppPath, string appDomainAppVirtualPath, Cache cache)
+        {
+			Framework.HttpRuntime.appDomainAppPath = appDomainAppPath;
+			Framework.HttpRuntime.appDomainAppVirtualPath = appDomainAppVirtualPath;
+			Framework.HttpRuntime.cache = cache;
+		}
+
+        public void Dispose()
+        {
+			Framework.HttpRuntime.appDomainAppPath = null;
+			Framework.HttpRuntime.appDomainAppVirtualPath = null;
+			Framework.HttpRuntime.cache = null;
+        }
+    }
+}
